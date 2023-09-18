d733c59927ea14dc79e2cdb3ad6c8a4eb9c46a93
Alona Harnyk <alyonagarnyk@gmail.com>
2023-09-18 22:18:11 +0300

    18.09
    18.09

---
+++ funcMethods.js
@@ -0,0 +1,144 @@
+'use strict'
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
+
+// invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+
+
+
+// // // BIND
+
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
+
+// const add = inventory.add.bind(inventory)
+
+// invokeInventoryAction('Medkit', add);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+
+
+
+// CALL
+
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+
+// console.log(this)
+
+//   console.log(`Invoking action on ${itemName}`);
+//   action.call(this, itemName);
+// };
+
+// invokeInventoryAction.call(inventory, 'Medkit', inventory.add);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+
+// invokeInventoryAction.call(inventory, 'Gas mask', inventory.remove);
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+
+// APPLY
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+
+console.log(this)
+
+  console.log(`Invoking action on ${itemName}`);
+  action.apply(this, [itemName]);
+};
+
+invokeInventoryAction.apply(inventory, ['Medkit', inventory.add]);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+
+invokeInventoryAction.apply(inventory, ['Gas mask', inventory.remove]);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
--- index.js
+++ index.js
@@ -1,315 +1,105 @@
-// const users = [
-//   {
-//     id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     eyeColor: "blue",
-//     friends: ["Sharron Pace"],
-//     isActive: false,
-//     balance: 2811,
-//     skills: ["ipsum", "lorem"],
-//     gender: "male",
-//     age: 37,
-//   },
-//   {
-//     id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     eyeColor: "blue",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     isActive: true,
-//     balance: 3821,
-//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-//     gender: "female",
-//     age: 34,
-//   },
-//   {
-//     id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     eyeColor: "green",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     isActive: false,
-//     balance: 3793,
-//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-//     gender: "male",
-//     age: 24,
-//   },
-//   {
-//     id: "249b6175-5c30-44c6-b154-f120923736f5",
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     eyeColor: "green",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     isActive: true,
-//     balance: 2278,
-//     skills: ["adipisicing", "irure", "velit"],
-//     gender: "female",
-//     age: 21,
-//   },
-//   {
-//     id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     eyeColor: "blue",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     isActive: true,
-//     balance: 3951,
-//     skills: ["ex", "culpa", "nostrud"],
-//     gender: "male",
-//     age: 27,
-//   },
-//   {
-//     id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     eyeColor: "brown",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     isActive: false,
-//     balance: 1498,
-//     skills: ["non", "amet", "ipsum"],
-//     gender: "male",
-//     age: 38,
-//   },
-//   {
-//     id: "e1bf46ab-7168-491e-925e-f01e21394812",
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     eyeColor: "brown",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     isActive: true,
-//     balance: 2764,
-//     skills: ["lorem", "veniam", "culpa"],
-//     gender: "female",
-//     age: 39,
-//   },
-// ];
+// 'use strict'
 
-// const ages = []
-// for (const user of users) {
-//   ages.push(user.age)
+// // **Function declaration**
+// function fnDeclaration() {
+//     console.log(this);
 // }
-// console.log(ages)
 
-// const ages = []
-// users.forEach(user => {ages.push(user.age)})
-// console.log(ages)
+// fnDeclaration()
 
-// const ages1 = users.map(user => user.age)
-// console.log(ages1)
 
-// const ages2 = users.map(({ age}) => age)
-// console.log(ages2)
+// // // **Function expression**
+// const fnExpression = function () {
+//     console.log(this);
+// }
 
-// const students = [
-//   { name: "Манго", courses: ["математика", "фізика"] },
-//   { name: "Полі", courses: ["інформатика", "математика"] },
-//   { name: "Ківі", courses: ["фізика", "біологія"] },
-// ];
+// fnExpression()
 
-// const a = students.map(({ courses }) => courses)
 
-// const b = a.flat()
+// // // **Arrow function**
+// const fnArrow = () => {
+//     console.log(this);
+// }
 
-// console.log(b)
-// console.log(a)
+// fnArrow()
 
-// const courses = students.flatMap(({courses}) => courses);
-// console.log(courses) // ['математика', 'фізика', 'інформатика', 'математика', 'фізика', 'біологія']
+// const person1 = {
+//     name: 'Olha',
+//     hello() {
+//         console.log(this)
+//         console.log(this.name)
+//     }
+// }
 
-// const students = [
-//   { name: "Манго", courses: ["математика", "фізика", [1, 2, [7, 8, [100, 700, [5, 10]]]]] },
-//   { name: "Полі", courses: ["інформатика", "математика", [3, 4, [9, 10]]] },
-//   { name: "Ківі", courses: ["фізика", "біологія", [5, 6]] },
-// ];
+// person1.hello()
 
-// const courses0 = students.map(({courses}) => courses);
+// const person2 = {
+//     name: 'Olha',
+//     hello: function() {
+//         console.log(this)
+//         console.log(this.name)
+//     }
+// }
 
-// console.log(courses0)
+// person2.hello()
 
-// const courses = students.flatMap(({courses}) => courses);
+// const person2 = {
+//     name: 'Olha',
+//     hello: () => {
+//         console.log(this)
+//     }
+// }
 
-// console.log(courses)
+// person2.hello()
 
-// const a = students.map(({ courses }) => courses)
 
-// console.log(a)
+// const person4 = {
+//     name: 'Olha',
+//         hello() {
+//         console.log(this)
 
-// console.log(a.flat(Infinity))
+//         const arrowFN = () => {
+//             console.log('arrowFN', this)
+//         }
 
-// const users1 = users.filter(({ balance }) => balance > 3000 )
-// console.log(users1)
+//         arrowFN()
+//     }
+// }
 
-// const students = [
-//   { name: "Манго", courses: ["математика", "фізика"] },
-//   { name: "Полі", courses: ["інформатика", "математика"] },
-//   { name: "Ківі", courses: ["фізика", "біологія"] },
-// ];
+// person4.hello()
 
-// const allCourses = students.flatMap(student => student.courses);
-// // ['математика', 'фізика', 'інформатика', 'математика', 'фізика', 'біологія'];
-// console.log(allCourses)
 
-// // const uniqueCourses = allCourses.filter(
-// //   (course, index, array) => {
-// //   console.log('#iteration', index + 1)
-// //   console.log('index', index)
-// //   console.log(array.indexOf(course))
-// //   return array.indexOf(course) === index
-// //   }
-// // );
 
-// const uniqueCourses = allCourses.filter(
-//   (course, index, array) => array.indexOf(course) === index
-// );
+// const bmw = {
+//     brand: 'BMW',
+//     speed: 100,
+// };
 
-// console.log(uniqueCourses)
 
-// const users2 = users.filter(({ name, balance }) => balance > 3000 && name[0] === "S")
-// console.log(users2)
+// const audi = {
+//     brand: 'Audi',
+//     speed: 80,
+//   };
 
-// const userFind = users.find(({ name, balance }) => balance > 3000 && name[0] === "S")
-// console.log(userFind)
 
-// const userFind = users.find(({ balance }) => balance > 3000 )
-// console.log(userFind)
+// function drive(action, city) {
+//     console.log(this)
+//     console.log(`${this.brand} їде зі швидкістю ${this.speed} км/год в ${city}. ${action}`);
+// }
 
-// console.log(users.indexOf(userFind))
+// drive('Увімкнути ближнє світло', 'Київ')
 
-// const index = users.findIndex(({  balance }) => balance > 3000 )
-// console.log(index)
+// drive.call(bmw, 'Увімкнути ближнє світло', 'Київ');
+// drive.call(audi, 'Увімкнути клімат-контроль', 'Львів');
 
-// const result1 = users.every(({ balance }) => balance > 1000)
+// drive.apply(bmw, ['Увімкнути ближнє світло', 'Київ']);
+// drive.apply(audi, ['Увімкнути клімат-контроль', 'Львів']);
 
-// console.log(result1)
 
-// const result2 = users.every(({age}) => age >= 20 && age <=30)
+const bmwDrive = drive.bind(bmw)
+bmwDrive('Увімкнути ближнє світло', 'Київ')
 
-// console.log(result2)
+// const audiDrive = drive.bind(audi)
+// audiDrive('Увімкнути ближнє світло', 'Київ')
 
-// const result3 = users.some(({ eyeColor }) => eyeColor === 'green')
-// console.log(result3)
 
-// const result4 = users.some(({friends}) => friends.length > 5)
-// console.log(result4)
-
-// const numbers = [2, 7, 3, 14, 6]
-
-// const total = numbers.reduce((previousValue, number) => {
-//   return previousValue + number;
-// }, 0);
-
-// console.log(total); // 32
-
-// const array = [2, 7, 3, 14, 6]
-// const init = 0
-// const total = array.reduce((previousValue, number, index) => {
-//   console.log('Iteration:', index + 1)
-//   console.log('previousValue', previousValue)
-//   console.log('number', number)
-//   const res = previousValue + number;
-//   console.log('res', res)
-//   return res;
-// }, init);
-
-// // console.log(total); // 32
-
-// const users = [
-//   {
-//     id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     eyeColor: "blue",
-//     friends: ["Sharron Pace"],
-//     isActive: false,
-//     balance: 2811,
-//     skills: ["ipsum", "lorem"],
-//     gender: "male",
-//     age: 37,
-//   },
-//   {
-//     id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     eyeColor: "blue",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     isActive: true,
-//     balance: 3821,
-//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-//     gender: "female",
-//     age: 34,
-//   },
-//   {
-//     id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     eyeColor: "green",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     isActive: false,
-//     balance: 3793,
-//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-//     gender: "male",
-//     age: 24,
-//   },
-//   {
-//     id: "249b6175-5c30-44c6-b154-f120923736f5",
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     eyeColor: "green",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     isActive: true,
-//     balance: 2278,
-//     skills: ["adipisicing", "irure", "velit"],
-//     gender: "female",
-//     age: 21,
-//   },
-//   {
-//     id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     eyeColor: "blue",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     isActive: true,
-//     balance: 3951,
-//     skills: ["ex", "culpa", "nostrud"],
-//     gender: "male",
-//     age: 27,
-//   },
-//   {
-//     id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     eyeColor: "brown",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     isActive: false,
-//     balance: 1498,
-//     skills: ["non", "amet", "ipsum"],
-//     gender: "male",
-//     age: 38,
-//   },
-//   {
-//     id: "e1bf46ab-7168-491e-925e-f01e21394812",
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     eyeColor: "brown",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     isActive: true,
-//     balance: 2764,
-//     skills: ["lorem", "veniam", "culpa"],
-//     gender: "female",
-//     age: 39,
-//   },
-// ];
-
-// const result1 = users.map(({balance}) => balance)
-// console.log(result1)
-// const result2 = result1.filter(el => el > 2500)
-// console.log(result2)
-// const result3 = result2.reduce((acc, item) => acc + item, 0)
-// console.log(result3)
-
-// const result = users
-//   .map(({ balance }) => balance)
-//   .filter((el) => el > 2500)
-//   .reduce((acc, item) => acc + item, 0);
-// console.log(result);
