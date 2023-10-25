6edbac27a3a6e0d92a922aafe04e7a4ad8546994
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-10-25 19:09:51 +0300

    update

--- src/js/01-promise-api.js
+++ src/js/01-promise-api.js
@@ -6,21 +6,6 @@
  *  - Promise.prototype.then(onResolve, onReject)
  */
 
-// promise.then(onFulfilled, onRejected);
-
-const promise = new Promise((resolve, reject) => {
-  const rand = Math.random();
-  setTimeout(() => {
-    if (rand > 0.5) {
-      resolve(rand);
-    } else {
-      reject('Sorry!');
-    }
-  }, 10);
-});
-
-// promise.then(onFulfilled).catch(onRejected);
-
 // function onFulfilled(result) {
 //   console.log('onFulfilled -> onFulfilled');
 //   console.log(`✅ ${result}`);
@@ -37,28 +22,28 @@ const promise = new Promise((resolve, reject) => {
  * Promise.prototype.finally()
  */
 
-promise
-  .then(value1 => {
-    console.log(value1, '-> promise1');
-    return [value1];
-  })
-  .catch(err => {
-    console.log('Error', err);
-    return Promise.reject('err');
-  })
-  .then(value2 => {
-    if (value2.length !== 0) {
-      console.log();
-    }
-    return 'food';
-  })
-  .then(value3 => {
-    return value3;
-  })
-  .then(value4 => {
-    console.log('value4->', value4);
-  })
-  .catch(() => {
-    console.log('last catch');
-  })
-  .finally(() => {});
+// promise
+//   .then(value1 => {
+//     console.log(value1, '-> promise1');
+//     return [value1];
+//   })
+//   .catch(err => {
+//     console.log('Error', err);
+//     return Promise.reject('err');
+//   })
+//   .then(value2 => {
+//     if (value2.length !== 0) {
+//       console.log();
+//     }
+//     return 'food';
+//   })
+//   .then(value3 => {
+//     return value3;
+//   })
+//   .then(value4 => {
+//     console.log('value4->', value4);
+//   })
+//   .catch(() => {
+//     console.log('last catch');
+//   })
+//   .finally(() => {});
